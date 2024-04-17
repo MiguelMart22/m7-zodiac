5b3e8bbac11f31003bb8531bd76aecb588ef2321
miguel.martinez <miguel.martinez35j@gmail.com>
2024-04-17 19:03:07 +0200

    added phrases of all times and zodiac signs

--- app/Http/Controllers/HoroscopeController.php
+++ app/Http/Controllers/HoroscopeController.php
@@ -26,25 +26,43 @@ class HoroscopeController extends Controller
             'Aquarius',
             'Pisces'
         ];
-        $times = ['today'];
-        foreach($times as $time) {
-            foreach($horoscopes as $horoscope) {
+        $times = ['Today', 'Yesterday', 'Week', 'Month'];
+        
+        foreach ($times as $time) {
+            // Obtener la fecha correcta según el tiempo especificado
+            switch ($time) {
+                case 'today':
+                    $date = date('d/m/Y');
+                    break;
+                case 'yesterday':
+                    $date = date('d/m/Y', strtotime('-1 day'));
+                    break;
+                case 'week':
+                    // Ajustar la fecha para obtener el inicio de la semana
+                    $date = date('d/m/Y', strtotime('last Monday'));
+                    break;
+                case 'month':
+                    // Ajustar la fecha para obtener el primer día del mes
+                    $date = date('d/m/Y', strtotime('first day of this month'));
+                    break;
+                default:
+                    $date = date('d/m/Y');
+                    break;
+            }
+            
+            foreach ($horoscopes as $horoscope) {
                 $text  = file_get_contents("https://www.astrology-zodiac-signs.com/api/call.php?time=$time&sign=$horoscope");
                 Horoscope::create([
-                    'date' => date('d/m/Y'),
+                    'date' => $date,
                     'lang' => 'en',
                     'sign' => $horoscope,
-                    'time' => 'today',
+                    'time' => $time,
                     'phrase' => $text,
                 ]);
             }
         }
-
-
-
-
-
-        }
+    }
+    
 
     /**
      * Display a listing of the resource.
